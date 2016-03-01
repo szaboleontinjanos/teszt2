6c854613327d9397d209d553d9262b912504be56
Levente Szabo <szabo.levente@zengo.eu>
2016-03-01 11:18:59 +0100

    adding nsobject category

---
+++ Pod/Classes/NSObject+Kismacska.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+Kismacska.h
+//  Pods
+//
+//  Created by Levente Szabo on 3/1/16.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (Kismacska)
+
+@end
---
+++ Pod/Classes/NSObject+Kismacska.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+Kismacska.m
+//  Pods
+//
+//  Created by Levente Szabo on 3/1/16.
+//
+//
+
+#import "NSObject+Kismacska.h"
+
+@implementation NSObject (Kismacska)
+
+@end
